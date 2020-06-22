600394e45d60a6df1812ac21a73cb8a564ccf5da
naomimorii <naomi.silva3@etec.sp.gov.br>
2020-06-22 19:28:26 -0300

    Inclusão do Projeto

---
+++ src/ctoffor/CtoFFor.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ctoffor;
+
+/**
+ *
+ * @author naomi
+ */
+public class CtoFFor {
+
+
+    public static void main(String[] args) {
+    int c;
+    double f;
+    
+    for (c=10; c<110; c=c+10)
+     {
+      f = (9*c + 160)/5;
+      System.out.print( c + " - " + f + " \n ");
+     }
+    }
+    
+}
